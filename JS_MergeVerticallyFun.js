2af3f812fc2bfffe089a92e49d6d6ce74f4c91ae
RomanBebenin <39234633+RomanBebenin@users.noreply.github.com>
2019-02-06 09:23:26 +1000

    Update JS_MergeVerticallyFun.js

--- JS_MergeVerticallyFun.js
+++ JS_MergeVerticallyFun.js
@@ -1,6 +1,9 @@
 function mergeCellsVert(table_id)
-//function deletes all the emppty cells and spans 
-// non-empty ones correspondingly for each cell
+// function formats a table in such a way that all the empty cells in a particular column are merged 
+// with the the most recent non-empty cell 
+// input: id of the table for which cells should be merged vertically
+// output: None
+// Exception handling: None
 {
 var myTable = document.getElementById(table_id);
 var column_num = myTable.rows[0].cells.length
